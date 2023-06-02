89a5f39cfa70043c2d94c966e4f3794a56b898f7
Saidafzal Kholkhujaev <saidafzal1209@gmail.com>
2023-06-02 13:58:00 +0500

    api

--- src/api.config.ts
+++ src/api.config.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:3000";
+export const API_URL = "auth-server-production-2f1e.up.railway.app";
 export const ENDPOINTS = {
   SIGNIN: "/auth/signin",
   SIGNUP: "/auth/signup",
